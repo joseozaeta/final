8926fe449a37e6a1bc55f743a1bd5e6068faf2b8
Jose Ozaeta <joseozaeta@Joses-MacBook-Pro-4.local>
2014-09-24 19:32:33 -0700

    final

--- js/scripts.js
+++ js/scripts.js
@@ -118,6 +118,87 @@ function prepareForms() {
     }
   }
 }
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+function insertAfter(newElement,targetElement) {
+  var parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement,targetElement.nextSibling);
+  }
+}
+
+function addClass(element,value) {
+  if (!element.className) {
+    element.className = value;
+  } else {
+    newClassName = element.className;
+    newClassName+= " ";
+    newClassName+= value;
+    element.className = newClassName;
+  }
+}
+
+function highlightPage() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("navigation")) return false;
+  var nav = document.getElementById("navigation");
+  var links = nav.getElementsByTagName("a");
+  for (var i=0; i<links.length; i++) {
+    var linkurl = links[i].getAttribute("href");
+    var currenturl = window.location.href;
+    if (currenturl.indexOf(linkurl) != -1) {
+      links[i].className = "here";
+      var linktext = links[i].lastChild.nodeValue.toLowerCase();
+      document.body.setAttribute("id",linktext);
+    }
+  }
+}
+function showSection(id) {
+  var divs = document.getElementsByTagName("div");
+  for (var i=0; i<divs.length; i++ ) {
+    if (divs[i].className.indexOf("section") == -1) continue;
+    if (divs[i].getAttribute("id") != id) {
+      divs[i].style.display = "none";
+    } else {
+      divs[i].style.display = "block";
+    }
+  }
+}
+
+function prepareInternalnav() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("internalnav")) return false;
+  var nav = document.getElementById("internalnav");
+  var links = nav.getElementsByTagName("a");
+  for (var i=0; i<links.length; i++ ) {
+    var sectionId = links[i].getAttribute("href").split("#")[1];
+    if (!document.getElementById(sectionId)) continue;
+    document.getElementById(sectionId).style.display = "none";
+    links[i].destination = sectionId;
+    links[i].onclick = function() {
+      showSection(this.destination);
+      return false;
+    }
+  }
+}
+
+addLoadEvent(prepareInternalnav);
+
+addLoadEvent(highlightPage);
 
 addLoadEvent(focusLabels);
 addLoadEvent(prepareForms);
